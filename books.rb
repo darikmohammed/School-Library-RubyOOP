f208f62e87f369d753f334aeedb27c9308c91f19
darikmohammed <r.m.darik@gmail.com>
2022-08-24 17:56:35 +0300

    Add relation between books and rental

--- books.rb
+++ books.rb
@@ -1,3 +1,5 @@
+require './rental.rb'
+
 # Books class
 class Books
   attr_accessor :title, :author
@@ -5,5 +7,10 @@ class Books
   def initialize(title, author)
     @title = title
     @author = author
+    @rentals = []
+  end
+
+  def add_rental(person, date)
+    Rental.new(self, person, date)
   end
 end
--- rental.rb
+++ rental.rb
@@ -2,7 +2,10 @@
 class Rental
   attr_accessor :date
 
-  def initialize(date)
+  def initialize(book , person, date)
     @date = date
+    @book = book
+    @person = person
+    book.rentals.push(self)
   end
 end
